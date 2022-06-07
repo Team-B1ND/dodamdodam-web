d00d0971a0c7ddc717f1a1e09e4d18b7d7fc9838
ldh3907 <ldh165163@gmail.com>
2022-06-07 12:29:40 +0900

    📑 ::폰트 교체

--- src/index.tsx
+++ src/index.tsx
@@ -11,4 +11,4 @@ root.render(<Root />);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(console.log);
