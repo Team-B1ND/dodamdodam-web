bcfb86abea7da90915ddd699799598f7c2b1ecc6
박상현 <104070112+Sanghyun0505@users.noreply.github.com>
2024-03-08 13:49:04 +0900

    refactor: 회원가입 test axios 적용

--- src/repository/member/member.repository.ts
+++ src/repository/member/member.repository.ts
@@ -1,19 +1,19 @@
 import config from "@src/config/config.json";
 import { MyMemberResponse } from "@src/types/member/member.type";
-import { dodamV6Axios } from "@src/lib/axios/customAxios";
+import { dodamTestAxios, dodamV6Axios } from "@src/lib/axios/customAxios";
 import { SignupParam } from "./member.param";
 import axios from "axios";
 
 class MemberRepository {
   public async postMemberSignUp(signupData: SignupParam): Promise<void> {
     await axios.post(
-      `${config.DODAM_SERVER_V6}/member/join-student`,
+      `${config.DODAM_TEST_SERVER}/member/join-student`,
       signupData
     );
   }
 
   public async getMyMember(): Promise<MyMemberResponse> {
-    const { data } = await dodamV6Axios.get<MyMemberResponse>("/members/my");
+    const { data } = await dodamTestAxios.get<MyMemberResponse>("/members/my");
     return data;
   }
 }
