98f6027eaf87a541a05ede592d94cd4d90065a22
박상현 <sanghyun9467@gmail.com>
2024-04-19 15:41:25 +0900

    Feature/#81 (#82)
    * feat: add firebase and remove @amplitude/analytics-browser dependency
    
    * refactor: remove existing GA
    
    * refactor: 필요없는 조건문 삭제

--- src/pages/homePage.tsx
+++ src/pages/homePage.tsx
@@ -1,27 +1,11 @@
+import { useLogGA } from "@src/hooks/ga/useLogGA";
 import PageTemplate from "../components/common/PageTemplate";
 import Home from "../components/home";
-import { init, track } from "@amplitude/analytics-browser";
-import { useEffect } from "react";
-import ReactGA from "react-ga4";
-import { pageView } from "../lib/ga/gtag";
 import useTokenCheck from "../hooks/auth/useTokenCheck";
 
 const HomePage = () => {
-  useEffect(() => {
-    if (!window.location.href.includes("localhost")) {
-      if (process.env.GOOGLE_ANALYTICS_ID) {
-        ReactGA.initialize(process.env.GOOGLE_ANALYTICS_ID);
-        pageView("메인페이지");
-      }
-
-      if (process.env.AMPLITUDE_KEY) {
-        init(process.env.AMPLITUDE_KEY);
-        track("메인페이지 접속");
-      }
-    }
-  }, []);
-
   useTokenCheck();
+  const { handleGALogEvent } = useLogGA();
 
   return (
     <PageTemplate>
