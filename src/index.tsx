94e296a6bdc461381595994d2e581ed4517c793d
해준 <127281057+ftery0@users.noreply.github.com>
2024-10-31 21:39:04 +0900

    fix :: env error

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,16 @@
 import ReactDOM from "react-dom/client";
 import Root from "./Root";
-import * as Sentry from "@sentry/react";
-import config from "./config/config.json";
+// import * as Sentry from "@sentry/react";
+// import config from "./config/config.json";
 
-Sentry.init({
-  dsn: config.SENTRY_DSN,
-  environment: process.env.NODE_ENV,
-  integrations: [new Sentry.BrowserTracing(), new Sentry.Replay()],
-  tracesSampleRate: 1.0,
-  replaysSessionSampleRate: 0.1,
-  replaysOnErrorSampleRate: 1.0,
-});
+// Sentry.init({
+//   dsn: config.SENTRY_DSN,
+//   environment: process.env.NODE_ENV,
+//   integrations: [new Sentry.BrowserTracing(), new Sentry.Replay()],
+//   tracesSampleRate: 1.0,
+//   replaysSessionSampleRate: 0.1,
+//   replaysOnErrorSampleRate: 1.0,
+// });
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
