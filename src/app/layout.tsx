1940e15f88353afbcd7fcb91abeab0590bf10c05
Cartridge0923 <pjmin0923@gmail.com>
2026-03-19 23:06:19 +0900

    feat: loading bar with cher1shrxd
    ^^

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import { ThemeSetter } from "@b1nd/dodam-design-system/next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { LoadingBar } from "@cher1shrxd/loading";
+import { colors } from "@b1nd/dodam-design-system";
 
 const pretendard = localFont({
   src: "../../public/font/PretendardVariable.woff2",
@@ -20,6 +22,7 @@ export default function RootLayout({
         <ThemeSetter />
       </head>
       <body className={`w-full h-screen ${pretendard.className}`}>
+        <LoadingBar color={colors.brand.primary}/>
         {children}
       </body>
     </html>
