58243501a2bbb9dfd96e15ddc1d787eecb58578c
Cartridge0923 <pjmin0923@gmail.com>
2026-03-19 21:58:16 +0900

    feat: svg turbopack config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  turbopack: {
+    rules: {
+      "*.svg": {
+        loaders: ["@svgr/webpack"],
+        as: "*.js",
+      },
+    },
+  },
 };
 
 export default nextConfig;
