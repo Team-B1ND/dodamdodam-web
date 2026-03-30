a87f1fb58722ff675fac73b29a1b56e458dbdfc7
dlskl <skyandme77@gmail.com>
2026-03-30 16:23:06 +0900

    feat: Schedule 루트 컴포넌트 및 스켈레톤 구현

---
+++ src/features/get-schedule/ui/index.tsx
@@ -0,0 +1,43 @@
+import ScheduleCalendar from "@/features/get-schedule/ui/ScheduleCalendar";
+import ScheduleList from "@/features/get-schedule/ui/ScheduleList";
+import useGetScheduleByDate from "@/features/get-schedule/model/useGetScheduleByDate";
+
+const Schedule = () => {
+  const {
+    currentMonth,
+    monthLabel,
+    scheduleEvents,
+    groupedSchedules,
+    moveMonth,
+  } = useGetScheduleByDate();
+
+  return (
+    <div className="w-full h-full flex flex-col xl:flex-row gap-4 items-start">
+      <ScheduleCalendar
+        currentMonth={currentMonth}
+        monthLabel={monthLabel}
+        scheduleEvents={scheduleEvents}
+        moveMonth={moveMonth}
+      />
+      <ScheduleList groupedSchedules={groupedSchedules} />
+    </div>
+  );
+};
+
+Schedule.Skeleton = () => {
+  return (
+    <div className="w-full h-full flex flex-col xl:flex-row gap-4">
+      <div className="large-container flex-1 flex flex-col gap-4">
+        <div className="w-40 h-8 rounded-extrasmall skeleton" />
+        <div className="w-full h-96 rounded-small skeleton" />
+      </div>
+      <div className="large-container w-full xl:w-88 flex flex-col gap-3">
+        <div className="w-24 h-7 rounded-extrasmall skeleton" />
+        <div className="w-full h-20 rounded-small skeleton" />
+        <div className="w-full h-20 rounded-small skeleton" />
+      </div>
+    </div>
+  );
+};
+
+export default Schedule;
