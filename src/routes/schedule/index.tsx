709d0086ff5deda77eb03b07f1b6672820be7a3f
dlskl <skyandme77@gmail.com>
2026-03-30 16:23:08 +0900

    feat: Schedule 경로 라우팅 추가

--- src/routes/schedule/index.tsx
+++ src/routes/schedule/index.tsx
@@ -1,9 +1,15 @@
-import { createFileRoute } from '@tanstack/react-router'
+import Schedule from "@/features/get-schedule/ui";
+import { createFileRoute } from "@tanstack/react-router";
+import { Suspense } from "react";
 
-export const Route = createFileRoute('/schedule/')({
+export const Route = createFileRoute("/schedule/")({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
-  return <div>Hello "/schedule/"!</div>
+  return (
+    <Suspense fallback={<Schedule.Skeleton />}>
+      <Schedule />
+    </Suspense>
+  );
 }
