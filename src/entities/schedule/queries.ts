06f004b2733350b0d4c0689071e5a15b0d978135
dlskl <skyandme77@gmail.com>
2026-03-30 19:46:13 +0900

    refactor: useQuery를 useSuspenseQuery로 변경

--- src/entities/schedule/queries.ts
+++ src/entities/schedule/queries.ts
@@ -1,10 +1,9 @@
 import { ScheduleApi } from "@/entities/schedule/api";
-import { keepPreviousData, useQuery } from "@tanstack/react-query";
+import { useSuspenseQuery } from "@tanstack/react-query";
 
 export const useGetSchedulesQuery = (date: string) =>
-  useQuery({
+  useSuspenseQuery({
     queryKey: ["schedule", date],
     queryFn: () => ScheduleApi.getSchedules(date),
-    placeholderData: keepPreviousData,
     staleTime: 1000 * 60,
   });
