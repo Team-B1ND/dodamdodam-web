b970a73e41a5e211d6819e1a769312d19bfa9f85
Cartridge0923 <pjmin0923@gmail.com>
2026-04-01 02:06:10 +0900

    feat: go login page on 403

--- src/shared/libs/api-client.ts
+++ src/shared/libs/api-client.ts
@@ -1,3 +1,6 @@
 import { createApiClient } from "@b1nd/api-client";
+import { redirectToLogin } from "@/shared/utils/redirect-to-login";
 
-export const apiClient = createApiClient(import.meta.env.VITE_API_URL!);
+export const apiClient = createApiClient(import.meta.env.VITE_API_URL!, {
+  onRefreshFailed: redirectToLogin,
+});
