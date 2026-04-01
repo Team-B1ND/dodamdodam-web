04b7367a92af3edbcbaa03ef177b4f8b81fc2d6a
Cartridge0923 <pjmin0923@gmail.com>
2026-04-01 17:34:16 +0900

    fix: remove admin banner and home banner file

--- src/routes/(role)/admin/_adminLayout/banner/index.tsx
+++ src/routes/(role)/admin/_adminLayout/banner/index.tsx
@@ -1,6 +1,4 @@
 import CreateBanner from '@/features/create-banner/ui'
-import AdminBanner from '@/features/get-banner/ui/AdminBanner'
-import QueryBoundary from '@/shared/ui/query-boundary'
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/(role)/admin/_adminLayout/banner/')({
@@ -10,9 +8,6 @@ export const Route = createFileRoute('/(role)/admin/_adminLayout/banner/')({
 function RouteComponent() {
   return (
     <div className='flex gap-4'>
-      <QueryBoundary pendingFallback={<AdminBanner.Skeleton />}>
-        <AdminBanner />
-      </QueryBoundary>
       <CreateBanner/>
     </div>
   )
