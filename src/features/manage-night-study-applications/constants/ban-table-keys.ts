e53426a3a8821f816c2e5eefcead01348826a800
lee090626 <hyeongseogi46@gmail.com>
2026-05-19 11:32:52 +0900

    feat: Dormitory 심자정지 페이지 정지 사유 표기 추가

--- src/features/manage-night-study-applications/constants/ban-table-keys.ts
+++ src/features/manage-night-study-applications/constants/ban-table-keys.ts
@@ -2,7 +2,7 @@ export const BAN_TABLE_KEYS: [string, string][] = [
   ["이름", "120px"],
   ["학반", "160px"],
   ["전화번호", "160px"],
-  ["", "FULL"],
+  ["심자 정지 사유", "FULL"],
   ["심자 정지 여부", "132px"],
 ];
 
